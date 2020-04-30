031c76d8a2f0ce1d99028f4f19ec71d7d103690a
Dmitry Kutsenko <d2emonium@gmail.com>
2020-04-30 14:56:10 +0300

    Add data for music server

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var news = require('../data/news.json');
+var tours = require('../data/tours.json');
+var events = require('../data/events.json');
+var videos = require('../data/videos.json');
+var article = require('../data/article.json');
+var stores = require('../data/stores.json');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    news,
+    tours,
+    events,
+    videos,
+    article,
+    stores: stores.image
+  });
 });
 
 module.exports = router;
