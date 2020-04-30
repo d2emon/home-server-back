e55a6ddb399d10e42ad6d4fe872cac13262cb41c
Dmitry Kutsenko <d2emonium@gmail.com>
2020-04-30 15:03:42 +0300

    Add albums data to backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,17 +7,53 @@ var events = require('../data/events.json');
 var videos = require('../data/videos.json');
 var article = require('../data/article.json');
 var stores = require('../data/stores.json');
+var albums = require('../data/albums.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.redirect('/index.html');
+});
+
+router.get('/index.:format?', function(req, res) {
   res.render('index', {
-    title: 'Express',
+    title: 'About',
     news,
     tours,
     events,
     videos,
     article,
     stores: stores.image
+  });
+});
+
+router.get('/audio.:format?', function(req, res) {
+  res.render('audio', {
+    title: 'Audio',
+    albums,
+  });
+});
+
+router.get('/video.:format?', function(req, res) {
+  res.render('video', {
+    title: 'Video'
+  });
+});
+
+router.get('/gallery.:format?', function(req, res) {
+  res.render('gallery', {
+    title: 'Gallery'
+  });
+});
+
+router.get('/tour-dates.:format?', function(req, res) {
+  res.render('tours', {
+    title: 'Tour Dates'
+  });
+});
+
+router.get('/contacts.:format?', function(req, res) {
+  res.render('contacts', {
+    title: 'Contacts'
   });
 });
 
